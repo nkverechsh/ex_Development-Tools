78ae8108fa1471329f6a8c6114d0a978a6a1c17e
nkverechsh <vereshagin-n@mail.ru>
2023-08-24 01:12:15 +0300

    3.2_Code Review

---
+++ js/startPage.js
@@ -0,0 +1,36 @@
+import game from './game.js';
+
+let level = loadLevel();
+
+function saveLevel(newLevel){
+    localStorage.setItem('level', JSON.stringify(newLevel))
+}
+
+function loadLevel(){
+    return localStorage.getItem('level') ? JSON.parse(localStorage.getItem('level')) : 'eazy';
+}
+
+function startPage() {
+    const app = document.querySelector('#add')
+    const html = `
+        <div class="complexity">
+        <h1 class="complexity__text">Выбери</br>сложность</h1>
+        <div class="complexity__level">
+            <button class="complexity__level_variant eazy">1</button>
+            <button class="complexity__level_variant medium">2</button>
+            <button class="complexity__level_variant hard">3</button>
+        </div>
+        <button class="start__game">
+            <p class="start__game_text">Старт</p>	
+        </button>
+        </div>
+    `
+    app.innerHTML = html
+
+    document.querySelector('.complexity__level_variant eazy').addEventListener('click', () => saveLevel('eazy'))
+    document.querySelector('.complexity__level_variant medium').addEventListener('click', () => saveLevel('medium'))
+    document.querySelector('.complexity__level_variant hard').addEventListener('click', () => saveLevel('hard'))
+    document.querySelector('.start__game').addEventListener('click', () => game())
+}
+
+startPage();
